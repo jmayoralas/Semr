036d43b3bab98e0d0034d964b5f07e5e9a9e774b
JLuis <yo@jluis.me>
2024-02-17 13:38:13 +0100

    Refactor Registers struct
    RefCell is used in order to allow interior mutability via specific setters.

--- src/cpu/regs.rs
+++ src/cpu/regs.rs
@@ -1,57 +1,135 @@
+use std::{borrow::{Borrow, BorrowMut}, cell::RefCell};
+
 #[derive(Debug)]
 pub struct RegisterSet {
-    pub a: Box<u8>,
-    pub f: Box<u8>,
-    pub b: Box<u8>,
-    pub c: Box<u8>,
-    pub d: Box<u8>,
-    pub e: Box<u8>,
-    pub h: Box<u8>,
-    pub l: Box<u8>,
+    a: RefCell<u8>,
+    f: RefCell<u8>,
+    b: RefCell<u8>,
+    c: RefCell<u8>,
+    d: RefCell<u8>,
+    e: RefCell<u8>,
+    h: RefCell<u8>,
+    l: RefCell<u8>,
 }
 
 impl RegisterSet {
     fn new() -> Self {
         Self {
-            a: Box::new(0),
-            f: Box::new(0),
-            b: Box::new(0),
-            c: Box::new(0),
-            d: Box::new(0),
-            e: Box::new(0),
-            h: Box::new(0),
-            l: Box::new(0),
+            a: RefCell::new(0),
+            f: RefCell::new(0),
+            b: RefCell::new(0),
+            c: RefCell::new(0),
+            d: RefCell::new(0),
+            e: RefCell::new(0),
+            h: RefCell::new(0),
+            l: RefCell::new(0),
         }
     }
 
-    pub fn af(&self) -> u16 {
-        (*self.a as u16) << 8 | *self.f as u16
+    pub fn a(&self) -> u8 {
+        *self.a.borrow()
     }
-    pub fn set_af(&mut self, value: u16) {
-        *self.a = (value >> 8) as u8;
-        *self.f = (value & 0x00FF) as u8;
+    pub fn f(&self) -> u8 {
+        *self.f.borrow()
+    }
+    pub fn b(&self) -> u8 {
+        *self.b.borrow()
+    }
+    pub fn c(&self) -> u8 {
+        *self.c.borrow()
+    }
+    pub fn d(&self) -> u8 {
+        *self.d.borrow()
+    }
+    pub fn e(&self) -> u8 {
+        *self.e.borrow()
+    }
+    pub fn h(&self) -> u8 {
+        *self.h.borrow()
+    }
+    pub fn l(&self) -> u8 {
+        *self.l.borrow()
+    }
+    pub fn set_a(&self, value: u8) {
+        *self.a.borrow_mut() = value;
+    }
+    pub fn set_f(&self, value: u8) {
+        *self.f.borrow_mut() = value;
+    }
+    pub fn set_b(&self, value: u8) {
+        *self.b.borrow_mut() = value;
+    }
+    pub fn set_c(&self, value: u8) {
+        *self.c.borrow_mut() = value;
+    }
+    pub fn set_d(&self, value: u8) {
+        *self.d.borrow_mut() = value;
+    }
+    pub fn set_e(&self, value: u8) {
+        *self.e.borrow_mut() = value;
+    }
+    pub fn set_h(&self, value: u8) {
+        *self.h.borrow_mut() = value;
+    }
+    pub fn set_l(&self, value: u8) {
+        *self.l.borrow_mut() = value;
+    }
+
+    pub fn af(&self) -> u16 {
+        (*self.a.borrow() as u16) << 8 | *self.f.borrow() as u16
+    }
+    pub fn set_af(&self, value: u16) {
+        *self.a.borrow_mut() = (value >> 8) as u8;
+        *self.f.borrow_mut() = (value & 0x00FF) as u8;
     }
     pub fn bc(&self) -> u16 {
-        (*self.b as u16) << 8 | *self.c as u16
+        (*self.b.borrow() as u16) << 8 | *self.c.borrow() as u16
     }
-    pub fn set_bc(&mut self, value: u16) {
-        *self.b = (value >> 8) as u8;
-        *self.c = (value & 0x00FF) as u8;
+    pub fn set_bc(&self, value: u16) {
+        *self.b.borrow_mut() = (value >> 8) as u8;
+        *self.c.borrow_mut() = (value & 0x00FF) as u8;
     }
     pub fn de(&self) -> u16 {
-        (*self.d as u16) << 8 | *self.e as u16
+        (*self.d.borrow() as u16) << 8 | *self.e.borrow() as u16
     }
-    pub fn set_de(&mut self, value: u16) {
-        *self.d = (value >> 8) as u8;
-        *self.e = (value & 0x00FF) as u8;
+    pub fn set_de(&self, value: u16) {
+        *self.d.borrow_mut() = (value >> 8) as u8;
+        *self.e.borrow_mut() = (value & 0x00FF) as u8;
     }
     pub fn hl(&self) -> u16 {
-        (*self.h as u16) << 8 | *self.l as u16
+        (*self.h.borrow() as u16) << 8 | *self.l.borrow() as u16
     }
-    pub fn set_hl(&mut self, value: u16) {
-        *self.h = (value >> 8) as u8;
-        *self.l = (value & 0x00FF) as u8;
+    pub fn set_hl(&self, value: u16) {
+        *self.h.borrow_mut() = (value >> 8) as u8;
+        *self.l.borrow_mut() = (value & 0x00FF) as u8;
     }
+
+    pub fn set_reg(&self, ix: u8, value: u8) -> Result<(), String> {
+        match ix {
+            0b000 => {self.set_b(value); Ok(())},
+            0b001 => {self.set_c(value); Ok(())},
+            0b010 => {self.set_d(value); Ok(())},
+            0b011 => {self.set_e(value); Ok(())},
+            0b100 => {self.set_h(value); Ok(())},
+            0b101 => {self.set_l(value); Ok(())},
+            0b111 => {self.set_a(value); Ok(())},
+            _ => Err("Bad register index".to_string())
+        }
+    }
+
+    pub fn get_reg(&self, ix: u8) -> Result<u8, String> {
+        match ix {
+            0b000 => Ok(self.b()),
+            0b001 => Ok(self.c()),
+            0b010 => Ok(self.d()),
+            0b011 => Ok(self.e()),
+            0b100 => Ok(self.h()),
+            0b101 => Ok(self.l()),
+            0b111 => Ok(self.a()),
+            _ => Err("Bad register index".to_string())
+        }
+    }
+
 }
 
 #[derive(Debug)]
@@ -80,14 +158,14 @@ mod test_register {
     #[test]
     fn test_regbank() {
         let mut bank = Registers::new();
-        *bank.main.a = 0xdd;
+        bank.main.set_a(0xdd);
         bank.alt.set_hl(0xccbb);
         bank.pc = 0x1234;
-        assert_eq!(*bank.main.a, 0xdd);
-        assert_eq!(*bank.main.h, 0x00);
-        assert_eq!(*bank.main.l, 0x00);
-        assert_eq!(*bank.alt.h, 0xcc);
-        assert_eq!(*bank.alt.l, 0xbb);
+        assert_eq!(bank.main.a(), 0xdd);
+        assert_eq!(bank.main.h(), 0x00);
+        assert_eq!(bank.main.l(), 0x00);
+        assert_eq!(bank.alt.h(), 0xcc);
+        assert_eq!(bank.alt.l(), 0xbb);
         assert_eq!(bank.pc, 0x1234);
     }
 }
